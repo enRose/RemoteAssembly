d4bc98783dbff1876d113c3666da047820eda63e
Yini <34562396+enRose@users.noreply.github.com>
2021-07-20 00:01:41 +1200

    move close button to the header

--- client/src/listing-detail/component/review-modal/review-modal.js
+++ client/src/listing-detail/component/review-modal/review-modal.js
@@ -5,59 +5,52 @@ import css from './review-modal-style.module.css'
 
 export const ReviewModal = (props) => {
   return (
-    <Modal
-      {...props}
-      size="lg"
-      aria-labelledby="contained-review-modal-title-vcenter"
-      centered
-    >
-      {
-        props.title &&
-        <Modal.Header closeButton>
-          <Modal.Title id="contained-review-modal-title-vcenter">
-            {props.title}
-          </Modal.Title>
-        </Modal.Header>
-      }
-      <Modal.Body>
+    <Container>
+      <Modal
+        {...props}
+        size="lg"
+        aria-labelledby="contained-review-modal-title-vcenter"
+        centered
+      >
         {
-          <Container>
+          <Modal.Header closeButton>
             <Row className={css['spacing-sm']}>
               <Col>
                 <span className={css['star-icon']}>
                   <i className={`bi bi-star-fill ${css['star-icon-colour']}`}>
                   </i>
-                  {`${props.stars} (${props.numOfReviews} reviews)`} 
+                  {`${props.stars} (${props.numOfReviews} reviews)`}
                 </span>
               </Col>
             </Row>
-          {props.reviews.map(x =>
+          </Modal.Header>
+        }
+        <Modal.Body scrollable={true}>
+          {
+            props.reviews.map(x =>
               <>
-              <Row className={css['spacing-sm']}>
-                <AvatarInitials
-                  firstName={x.userName}>
-                  <Col sm={1}>
-                  </Col>
+                <Row className={css['spacing-sm']}>
+                  <AvatarInitials
+                    firstName={x.userName}>
+                    <Col sm={1}>
+                    </Col>
 
-                </AvatarInitials>
-                <Col>
-                  <div>{x.userName}</div>
-                  <span>Member since {x.memberSince}</span>
-                </Col>
-              </Row>
-              <Row>
-                <Col>
-                <p>{x .utterance}</p>
-                </Col>
-              </Row>
-            </>
-          )}
-          </Container>
-      }
-      </Modal.Body>
-      <Modal.Footer>
-        <Button onClick={props.onHide}>Close</Button>
-      </Modal.Footer>
-    </Modal>
+                  </AvatarInitials>
+                  <Col>
+                    <div>{x.userName}</div>
+                    <span>Member since {x.memberSince}</span>
+                  </Col>
+                </Row>
+                <Row>
+                  <Col>
+                    <p>{x.utterance}</p>
+                  </Col>
+                </Row>
+              </>
+            )
+          }
+        </Modal.Body>
+      </Modal>
+    </Container>
   )
 }
