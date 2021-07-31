4b5c2a4b8e49cbcb5a4b690b90c69eb4f5396bc7
Yini <34562396+enRose@users.noreply.github.com>
2021-08-01 01:05:27 +1200

    add onScroll to review modal scroll event listener

--- client/src/listing-detail/component/review-modal/review-modal.js
+++ client/src/listing-detail/component/review-modal/review-modal.js
@@ -1,12 +1,22 @@
+import React, { useEffect, useRef, useState } from "react"
 import { Row, Col, Modal, Button, Container } from 'react-bootstrap'
 import { AvatarInitials } from '../avatar-initials'
 import css from './review-modal-style.module.css'
 
-
 export const ReviewModal = (props) => {
+  const modalBody = useRef(null)
+  const [onScrollRegistered, setOnScrollRegistered] = useState(false)
+
+  const onScroll = () => {
+    const modal = document.querySelectorAll('div.modal.show')[0]
+
+    modal && modal.addEventListener('scroll', () => console.log('scrolling'))
+  }
+
   return (
     <Container>
       <Modal
+        onEntered={onScroll}
         {...props}
         size="lg"
         aria-labelledby="contained-review-modal-title-vcenter"
@@ -14,7 +24,7 @@ export const ReviewModal = (props) => {
       >
         {
           <Modal.Header closeButton>
-            <Row className={css['spacing-sm']}>
+            <Row>
               <Col>
                 <span className={css['star-icon']}>
                   <i className={`bi bi-star-fill ${css['star-icon-colour']}`}>
@@ -25,30 +35,33 @@ export const ReviewModal = (props) => {
             </Row>
           </Modal.Header>
         }
-        <Modal.Body scrollable={true}>
-          {
-            props.reviews.map(x =>
-              <>
-                <Row className={css['spacing-sm']}>
-                  <AvatarInitials
-                    firstName={x.userName}>
-                    <Col sm={1}>
-                    </Col>
+        <Modal.Body >
+          <div>
+            {
+              props.reviews.map(x =>
+                <>
+                  <Row className={css['spacing-sm']}>
+                    <AvatarInitials
+                      firstName={x.userName}>
+                      <Col sm={1}>
+                      </Col>
 
-                  </AvatarInitials>
-                  <Col>
-                    <div>{x.userName}</div>
-                    <span>Member since {x.memberSince}</span>
-                  </Col>
-                </Row>
-                <Row>
-                  <Col>
-                    <p>{x.utterance}</p>
-                  </Col>
-                </Row>
-              </>
-            )
-          }
+                    </AvatarInitials>
+                    <Col>
+                      <div>{x.userName}</div>
+                      <span>Member since {x.memberSince}</span>
+                    </Col>
+                  </Row>
+                  <Row>
+                    <Col>
+                      <p>{x.utterance}</p>
+                    </Col>
+                  </Row>
+                </>
+              )
+
+            }
+          </div>
         </Modal.Body>
       </Modal>
     </Container>
