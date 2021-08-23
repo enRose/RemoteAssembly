8c7b883c74433d58cbd36ab3f9b9a2ab710169d8
Yini <34562396+enRose@users.noreply.github.com>
2021-08-23 23:54:36 +1200

    booking calendar style

--- client/src/listing-detail/component/booking-calendar/index.js
+++ client/src/listing-detail/component/booking-calendar/index.js
@@ -1,10 +1,13 @@
 import React, { useState } from 'react'
-import { DatePicker, Divider, Card, Affix, InputNumber, Form, Input, Button, Select } from 'antd'
+import { DatePicker, Divider, Card, Affix, InputNumber, Form, Input, Button, Collapse } from 'antd'
 import {
   StarFilled,
 } from '@ant-design/icons'
+import css from './booking-calendar-style.module.css'
+
 
 const { RangePicker } = DatePicker
+const { Panel } = Collapse
 
 export const BookingCalendar = (props) => {
   const [form] = Form.useForm()
@@ -12,9 +15,9 @@ export const BookingCalendar = (props) => {
   const layout = {
     labelCol: {
       xs: { span: 24 },
-      sm: { span: 9 },
+      sm: { span: 10 },
     },
-    tail: { 
+    tail: {
       xs: { span: 24 },
       sm: { span: 24 },
     },
@@ -23,13 +26,17 @@ export const BookingCalendar = (props) => {
       sm: { span: 24 },
     },
   }
-  
+
   function onNumOfStudentsInputChange(value) {
     console.log('changed', value);
   }
 
   const onFinish = (values) => {
     console.log('Success:', values);
+  }
+
+  function callback(key) {
+    console.log(key);
   }
 
   return (
@@ -46,23 +53,28 @@ export const BookingCalendar = (props) => {
             <RangePicker />
           </Form.Item>
 
-          <Form.Item label="Age 2 - 5">
-            <InputNumber min={1} max={10} defaultValue={3} onChange={onNumOfStudentsInputChange} />
-          </Form.Item>
+          <Collapse className={css['small-margin-bottom']} defaultActiveKey={['1']} onChange={callback}>
+            <Panel header="This is panel header 1" key="1">
+              <Form.Item label="Age 2 - 5">
+                <InputNumber min={1} max={10} defaultValue={3} onChange={onNumOfStudentsInputChange} />
+              </Form.Item>
 
-          <Form.Item label="Age 6 - 10">
-            <InputNumber min={1} max={10} defaultValue={3} onChange={onNumOfStudentsInputChange} />
-          </Form.Item>
+              <Form.Item label="Age 6 - 10">
+                <InputNumber min={1} max={10} defaultValue={3} onChange={onNumOfStudentsInputChange} />
+              </Form.Item>
 
-          <Form.Item label="Age 11 - 16">
-            <InputNumber min={1} max={10} defaultValue={3} onChange={onNumOfStudentsInputChange} />
+              <Form.Item className={css['no-margin-bottom']} label="Age 11 - 16">
+                <InputNumber min={1} max={10} defaultValue={3} onChange={onNumOfStudentsInputChange} />
+              </Form.Item>
+            </Panel>
+          </Collapse>
+
+          <Form.Item className={css['no-margin-bottom']} wrapperCol={{ ...layout.tail }}>
+            <Button type="primary" htmlType="submit" block>
+              Reserve
+            </Button>
           </Form.Item>
         </Form>
-        <Form.Item wrapperCol={{ ...layout.tail }}>
-        <Button type="primary" htmlType="submit" block>
-          Reserve
-        </Button>
-      </Form.Item>
       </Card>
     </Affix>
   )
