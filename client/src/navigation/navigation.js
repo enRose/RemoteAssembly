89c20ced6b9164a3ca73e55a511f97a05b5a72e2
Yini <34562396+enRose@users.noreply.github.com>
2021-09-06 00:04:12 +1200

    navigation header style

--- client/src/navigation/navigation.js
+++ client/src/navigation/navigation.js
@@ -4,36 +4,48 @@ import Register from '../register'
 import { Listings } from '../listings'
 import { ListingDetail } from '../listing-detail'
 import css from './nav-style.module.css'
-import { Layout, Menu } from 'antd'
-import {Theme} from '../theme-manager/theme'
+import { Layout, Menu, Input, Row, Col, Divider } from 'antd'
+import { SearchOutlined } from '@ant-design/icons'
+import { Theme } from '../theme-manager/theme'
 
 const { Header, Content, Footer } = Layout
 
-//Attribution icon
-{/* <div>Icons made by <a href="https://www.flaticon.com/authors/smashicons" title="Smashicons">Smashicons</a> from <a href="https://www.flaticon.com/" title="Flaticon">www.flaticon.com</a></div> */}
-
 const Navigation = () => {
+  const onSearch = value => console.log(value)
+
   return (
     <Layout>
       <Header className={css[Theme]}
-        style={{ 
-        position: 'fixed', 
-        zIndex: 1, 
-        width: '100%'
+        style={{
+          position: 'fixed',
+          zIndex: 1,
+          width: '100%'
         }}>
-        <NavLink className={css.logo} to="/"><img
-          alt=""
-          src={fern}
-          width="30"
-          height="30"
-          className="d-inline-block align-top"
-        />{' '}</NavLink>
-        <Menu theme={Theme} mode="horizontal" defaultSelectedKeys={['1']}>
-          <Menu.Item key="1"><NavLink to="/listings">All Classes</NavLink></Menu.Item>
-          <Menu.Item key="2"><NavLink to="/register">register</NavLink></Menu.Item>
-        </Menu>
-      </Header>
+        <Row justify='space-between'>
+          <Col xs={0} sm={0} md={2} span={2}>
+            <NavLink className={css.logo} to="/">
+              <img
+                alt=""
+                src={fern}
+                width="30"
+                height="30"
+                className="d-inline-block align-top"
+              />{''}</NavLink>
+            <Divider type="vertical" />
+          </Col>
 
+          <Col span={12}>
+            <Input placeholder="search a class" prefix={<SearchOutlined />} />
+          </Col>
+          <Col span={10}>
+            <Menu theme={Theme} mode="horizontal" defaultSelectedKeys={['1']}>
+              <Menu.Item key="1"><NavLink to="/listings">All Classes</NavLink></Menu.Item>
+              <Menu.Item key="2"><NavLink to="/register">register</NavLink></Menu.Item>
+            </Menu>
+          </Col>
+
+        </Row>
+      </Header>
       <Content className={css['site-layout']} style={{ padding: '0 50px', marginTop: 64 }}>
         <div className={css['site-layout-background']} style={{ padding: 24, minHeight: 380 }}>
           <Switch>
@@ -44,7 +56,10 @@ const Navigation = () => {
           </Switch>
         </div>
       </Content>
-      <Footer style={{ textAlign: 'center' }}>Remote Assembly ©2021 Created by Barin</Footer>
+      <Footer style={{ textAlign: 'center' }}>
+        Remote Assembly ©2021 Created by Barin | Icons by <a href='https://www.flaticon.com/authors/smashicons'>
+          Flaticon</a>
+      </Footer>
     </Layout>
   )
 }
